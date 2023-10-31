ae4ee88d2d00bcf3cb30a177b7d80f210175e111
Marcus Ellested <46597619+Marcus4420@users.noreply.github.com>
2023-10-31 21:36:00 +0100

    access to sub path

--- app/courses/[course]/page.tsx
+++ app/courses/[course]/page.tsx
@@ -1,3 +1,11 @@
-export default function Page({ params }: { params: { course: string } }) {
-  return <div>My Post: {params.course}</div>
+'use client'
+ 
+import { usePathname } from 'next/navigation'
+ 
+export default function ExampleClientComponent({ params }: { params: { course: string } }) {
+  const pathname = usePathname()
+  const subPathIndexStart = pathname.indexOf('/', 2);
+  const subPath = pathname.substring(subPathIndexStart+1, pathname.length);
+  console.log(subPath);
+  return <p>{subPath}</p>
 }
